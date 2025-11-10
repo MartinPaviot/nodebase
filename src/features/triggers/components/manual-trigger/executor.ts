0657559c946007fae93eeb2b11a5ffe5f5975596
Martin Paviot <martin.paviot@outlook.com>
2025-11-10 23:24:05 +0100

    21: node realtime

--- src/features/triggers/components/manual-trigger/executor.ts
+++ src/features/triggers/components/manual-trigger/executor.ts
@@ -1,17 +1,29 @@
 import type { NodeExecutor } from "@/features/executions/types";
+import { manualTriggerChannel } from "@/inngest/channels/manual-trigger";
 
 type ManualTriggerData = Record<string, unknown>;
 
 export const manualTriggerExecutor: NodeExecutor<ManualTriggerData> = async ({
-    nodeId,
-    context,
-    step,
-}) => {
-// TODO: Publish "loading" state for manualt trigger
+        nodeId,
+        context,
+        step,
+        publish,
+    }) => {
+        await publish(
+            manualTriggerChannel().status({
+                nodeId,
+                status: "loading",
+            }),
+        );
 
-const result = await step.run("manual-trigger", async () => context);
+    const result = await step.run("manual-trigger", async () => context);
 
-// TODO: Publish "success" state for manual trigger
+    await publish(
+            manualTriggerChannel().status({
+                nodeId,
+                status: "success",
+            }),
+        );
 
     return result;
 };
