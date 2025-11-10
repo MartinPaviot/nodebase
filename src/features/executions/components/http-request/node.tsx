d790ad8c540f3b5dc0012a18d0db015adda8367e
Martin Paviot <martin.paviot@outlook.com>
2025-11-10 19:15:56 +0100

    19: Node Variables

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -7,6 +7,7 @@ import { BaseExecutionNode} from "../base-execution-node";
 import { HttpRequestFormValues, HttpRequestDialog } from "./dialog";
 
 type HttpRequestNodeData = {
+    vairableName?: string;
     endpoint?: string;
     method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
